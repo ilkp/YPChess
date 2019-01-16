aa848ca2e86c34ca52c2c92d9fba1ea8fc489a2b
ilkp <ilkka.pokkinen@gmail.com>
2019-01-16 15:26:38 +0200

    Some updates

--- YPChessVisualStudio/YPChessVisualStudio/UI.h
+++ YPChessVisualStudio/YPChessVisualStudio/UI.h
@@ -1,8 +1,21 @@
 #pragma once
+
+#include "State.h"
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class UI
 {
+private:
+	State* state;
+	HANDLE outputH;
+
 public:
-	UI();
+	UI(State* state);
 	~UI();
+	void drawBoard();
 };
 
