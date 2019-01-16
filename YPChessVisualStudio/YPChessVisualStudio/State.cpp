cb5d43ba89abe1358170d4f9781de9f499129797
Joona Yli-Villamo <joonayv@gmail.com>
2019-01-16 15:29:33 +0200

    Päivitys

--- YPChessVisualStudio/YPChessVisualStudio/State.cpp
+++ YPChessVisualStudio/YPChessVisualStudio/State.cpp
@@ -1,12 +1,43 @@
 #include "State.h"
+#include <iostream>
+#include <wstring>
+using namespace std;
 
 
 
 State::State()
 {
+	board = new Piece*[8][8];
 }
 
 
 State::~State()
 {
+}
+
+Piece State::getBoard()
+{
+	return Piece();
+}
+
+void State::setBoard(Piece * board)
+{
+}
+
+void State::setSquare(int x, int y, Piece* piece)
+{
+	board[x][y] = piece;
+}
+
+int State::getPiece(int x, int y)
+{
+	if (board[x][y] == NULL)
+	{
+		return NULL;
+	}
+	else;
+	{
+		return ;
+	}
+	
 }
--- YPChessVisualStudio/YPChessVisualStudio/State.h
+++ YPChessVisualStudio/YPChessVisualStudio/State.h
@@ -1,8 +1,51 @@
 #pragma once
+#include "Piece.h"
+
 class State
 {
+private:
+
+	Piece* VK;
+	Piece* VD;
+	Piece* VT;
+	Piece* VL;
+	Piece* VR;
+	Piece* VS;
+	Piece* MK;
+	Piece* MD;
+	Piece* MT;
+	Piece* ML;
+	Piece* MR;
+	Piece* MS;
+
+	/*
+	VK, L"\u2654	
+	VD, L"\u2655	
+	VT, L"\u2656	 
+	VL, L"\u2657	
+	VR, L"\u2658	
+	VS, L"\u2659"
+	MK, L"\u265A	
+	MD, L"\u265B
+	MT, L"\u265C”
+	ML, L"\u265D"
+	MR, L"\u265E"
+	MS, L"\u265F"
+	*/
+
+	Piece* board[8][8];
+
+
 public:
 	State();
 	~State();
+
+	Piece getBoard();
+
+	void setBoard(Piece* board);
+
+	void setSquare(int x, int y, Piece* piece);
+
+	int getPiece(int x, int y);
 };
 
